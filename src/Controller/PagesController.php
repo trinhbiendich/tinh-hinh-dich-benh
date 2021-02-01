b753cec46ee84f2cda5f99c77bc115535869c3ba
Sac Le <le.sinh@mail.i-on.net>
2021-02-01 12:00:24 +0700

    update code

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -1,19 +1,5 @@
 <?php
 declare(strict_types=1);
-
-/**
- * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- *
- * Licensed under The MIT License
- * For full copyright and license information, please see the LICENSE.txt
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- * @link      https://cakephp.org CakePHP(tm) Project
- * @since     0.2.9
- * @license   https://opensource.org/licenses/mit-license.php MIT License
- */
 namespace App\Controller;
 
 use Cake\Core\Configure;
@@ -22,13 +8,6 @@ use Cake\Http\Exception\NotFoundException;
 use Cake\Http\Response;
 use Cake\View\Exception\MissingTemplateException;
 
-/**
- * Static content controller
- *
- * This controller will render views from templates/Pages/
- *
- * @link https://book.cakephp.org/4/en/controllers/pages-controller.html
- */
 class PagesController extends AppController
 {
     /**
@@ -48,6 +27,7 @@ class PagesController extends AppController
         if (!$path) {
             return $this->redirect('/');
         }
+        print_r($path);
         if (in_array('..', $path, true) || in_array('.', $path, true)) {
             throw new ForbiddenException();
         }
@@ -69,5 +49,9 @@ class PagesController extends AppController
             }
             throw new NotFoundException();
         }
+    }
+
+    public function pages() {
+        return $this->render("home");
     }
 }
