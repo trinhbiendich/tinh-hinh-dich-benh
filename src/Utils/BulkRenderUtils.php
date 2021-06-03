c8b420a61ab0f6437d74c6bf5b7abb01e4237cdf
Sac Le <le.sinh@mail.i-on.net>
2021-06-03 16:00:32 +0700

    update bulk process

--- src/Utils/BulkRenderUtils.php
+++ src/Utils/BulkRenderUtils.php
@@ -11,12 +11,32 @@ class BulkRenderUtils {
 
     public static function processForEvent($event, array $params, array $dataFromStorage, $data, ApiController $triggerEvent) {
         switch ($event) {
+            case 'bulk_photos':
+                self::bulkInsertPhotos($params[0], $dataFromStorage, $data, $triggerEvent);
+                return;
             case 'bulk':
                 self::bulkInsert($params[0], $dataFromStorage, $data, $triggerEvent);
                 return;
             default:
                 $triggerEvent->error('there is no event with name ' . $event);
         }
+    }
+
+    private static function bulkInsertPhotos($params, array $dataFromStorage, array $data, ApiController $triggerEvent) {
+        $counter = 0;
+        $ids = [];
+        foreach ($data as $item) {
+            if (!isset($item['id']) || empty($item['id'])) {
+                continue;
+            }
+            Cache::write("photos_" . $item['id'], $item);
+            array_push($ids, $item['id']);
+            $counter++;
+        }
+
+        $dataFromStorage = array_unique(array_merge($dataFromStorage, $ids));
+        Cache::write($params, $dataFromStorage);
+        $triggerEvent->success(["msg" => "save success $counter items into storage", "ids" => $ids]);
     }
 
     private static function bulkInsert($params, array $dataFromStorage, array $data, ApiController $triggerEvent) {
